33a55914e095d4f4764a92712b3e6a9ee1c60f66
nevikw39 <nevik.w39@gmail.com>
2021-06-24 20:55:44 +0800

    Deploy: main -> master

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:nevikw39/ptt-id.git main:gh-pages
+git push -f git@github.com:nevikw39/ptt-id.git master:gh-pages
 
 cd -

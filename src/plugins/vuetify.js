23c236b06928b583a63da4f5e4b403baa57f52ff
nevikw39 <nevik.w39@gmail.com>
2021-06-24 20:47:54 +0800

    Set theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,24 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
+import colors from 'vuetify/lib/util/colors';
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        dark: true,
+        default: 'dark',
+        themes: {
+            dark: {
+                primary: colors.teal,
+                secondary: colors.cyan,
+                accent: colors.amber,
+                error: '#E9693D',
+                info: '#8DD4E8',
+                success: '#198964',
+                warning: '#F0E87D',
+            },
+        },
+    },
 });

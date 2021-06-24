efb929eb9305bee404bd45192d8a20b6ec449d79
nevikw39 <nevik.w39@gmail.com>
2021-06-25 03:11:44 +0800

    Main

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,20 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>ptt ID</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
       <PttId />
     </v-main>
+
+    <v-footer color="primary lighten-1" padless>
+      <v-row justify="center" no-gutters>
+        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} — <strong>nevikw39</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
--- src/components/PttId.vue
+++ src/components/PttId.vue
@@ -1,72 +1,72 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
+      <!-- <v-col cols="12">
         <v-img
           :src="require('../assets/logo.svg')"
           class="my-3"
           contain
           height="200"
         />
-      </v-col>
+      </v-col> -->
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
+        <h1 class="display-2 font-weight-bold mb-3">歡迎來到 ptt ID</h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
+          輸入 ptt 上的 id, 即列出其最近上站的 10 個 IP
+          地址、時間、國家與是否為跳板。
         </p>
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+        <h2 class="headline font-weight-bold mb-3">工商：ptt IP</h2>
 
         <v-row justify="center">
+          <p class="subheading font-weight-regular">
+            一款 userscript, 令您只要雙擊選取 ptt 上 IP 便可即時取得相關資訊。
+          </p>
           <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
+            href="https://gist.github.com/nevikw39/d7a08f637919348106f6a35d35a62d3f"
+            lass="subheading mx-3"
             target="_blank"
+            >前往了解</a
           >
-            {{ next.text }}
-          </a>
         </v-row>
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
+        <h2 class="headline font-weight-bold mb-3">輸入</h2>
 
         <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
+          <v-col cols="12" sm="6" md="3">
+            <input placeholder="ID" v-model.lazy="id" />
+          </v-col>
         </v-row>
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
+        <h2 class="headline font-weight-bold mb-3">輸出</h2>
+
+        <v-alert prominent type="error" v-if="error">{{ rrore }}</v-alert>
 
         <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
+          <v-data-table :headers="headers" :items="spur" class="elevation-8">
+            <template slot="item.rank" scope="props">
+              {{ props.index }}
+            </template>
+            <template v-slot:item.context="{ item }">
+              <v-chip
+                :color="color(item.context)"
+                light
+                v-html="context(item.context, item.ip)"
+              >
+              </v-chip>
+            </template>
+            <template v-slot:item.last_seen_at="{ item }">
+              {{ date(item.last_seen_at) }}
+            </template>
+          </v-data-table>
         </v-row>
       </v-col>
     </v-row>
@@ -78,56 +78,92 @@ export default {
   name: "PttId",
 
   data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader",
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify",
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify",
-      },
+    id: "",
+    plytic: [],
+    spur: [],
+    headers: [
+      { text: "#", value: "rank" },
+      { text: "IP", value: "ip" },
+      { text: "國家", value: "country" },
+      { text: "時間", value: "last_seen_at" },
+      { text: "Context", value: "context" },
     ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com",
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com",
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify",
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs",
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify",
-      },
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer",
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-      },
-    ],
+    error: null,
   }),
+
+  watch: {
+    id: function (id) {
+      const parser = new window.DOMParser();
+      fetch(
+        "https://thingproxy.freeboard.io/fetch/https://www.plytic.com/api/v1/authors/" +
+          id.toLowerCase() +
+          "/source_ips"
+      )
+        .then((r) => r.json())
+        .then((j) => {
+          this.plytic = j.recent_source_ips;
+          this.plytic
+            .forEach((element) => {
+              element.context = "--";
+              if (element.country != "TW")
+                fetch(
+                  "https://thingproxy.freeboard.io/fetch/https://spur.us/context/" +
+                    element.ip
+                )
+                  .then((r) => r.text())
+                  .then((t) => {
+                    element.context = parser.parseFromString(
+                      t,
+                      "text/html"
+                    ).title;
+                    this.spur = this.plytic;
+                  });
+              element.country =
+                String.fromCodePoint(
+                  ...element.country
+                    .toUpperCase()
+                    .split("")
+                    .map((char) => 127397 + char.charCodeAt())
+                ) + element.country;
+              element.last_seen_at = new Date(element.last_seen_at);
+            })
+            .catch((e) => (this.error = e));
+        });
+    },
+  },
+
+  computed: {
+    //
+  },
+
+  methods: {
+    color: function (x) {
+      return x.includes("esidential") || x.includes("ikely")
+        ? "danger"
+        : x.includes("VPN") || x.includes("roxy")
+        ? "error"
+        : "info";
+    },
+    context: function (x, y) {
+      if (/\(.*\)/.test(x))
+        return (
+          '<a target="_blank" href="https://spur.us/context/' +
+          y +
+          '">' +
+          x.match(/\(.*\)/)[0].slice(2, -2) +
+          "</a>"
+        );
+      else return "--";
+    },
+    date: function (x) {
+      return new Intl.DateTimeFormat("zh-TW", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour12: false,
+        timeZone: "Asia/Taipei",
+      }).format(x);
+    },
+  },
 };
 </script>
